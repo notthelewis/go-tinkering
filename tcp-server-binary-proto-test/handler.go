908372e69e3c2ec90ad9bbaa26cd0cd0839f77df
Lewis Farnworth <lewfar99@gmail.com>
2023-05-14 21:27:01 +0100

    Go fmt

--- tcp-server-binary-proto-test/handler.go
+++ tcp-server-binary-proto-test/handler.go
@@ -4,26 +4,27 @@ import (
 	"fmt"
 	"net"
 )
+
 func Handle_request(cnx net.Conn) {
 	fmt.Printf("handle_request::%v\n", cnx.RemoteAddr().String())
-    
-    helloMessageBuffer := make([]byte, HELLO_MSG_LEN_IN_BYTES)
 
-    _, e := cnx.Read(helloMessageBuffer)
+	helloMessageBuffer := make([]byte, HELLO_MSG_LEN_IN_BYTES)
 
-    if e != nil {
-        fmt.Printf("error::handle_request::%v\n", e.Error())
-        cnx.Close()
-        return
-    }
+	_, e := cnx.Read(helloMessageBuffer)
 
-    hello, e := parse_hello(helloMessageBuffer)
-    if e != nil {
-        cnx.Close()
-        return
-    }
+	if e != nil {
+		fmt.Printf("error::handle_request::%v\n", e.Error())
+		cnx.Close()
+		return
+	}
 
-    fmt.Printf("Recieved hello: %+v\n", hello)
-    cnx.Write([]byte{0})
-    cnx.Close()
+	hello, e := parse_hello(helloMessageBuffer)
+	if e != nil {
+		cnx.Close()
+		return
+	}
+
+	fmt.Printf("Recieved hello: %+v\n", hello)
+	cnx.Write([]byte{0})
+	cnx.Close()
 }

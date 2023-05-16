5ffbc09222cef422521bfa107a1fd50cf6a60809
Lewis Farnworth <lewfar99@gmail.com>
2023-05-16 19:54:11 +0100

    Added the method to read entire file into memory instead of using bufreader

--- book/1.2-cmdline/1.3-duplicate-lines/main.go
+++ book/1.2-cmdline/1.3-duplicate-lines/main.go
@@ -1,9 +1,11 @@
-package main 
+package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func find_duplicates1() {
@@ -53,6 +55,28 @@ func find_duplicates2() {
     }
 }
 
+func find_duplicates3() {
+    counts := make(map[string]int)
+    for _, filename := range os.Args[1:] {
+        data, err := ioutil.ReadFile(filename)
+
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "duplicates3: %\n", err)
+            continue
+        }
+
+        for _, line := range strings.Split(string(data), "\n") {
+            counts[line]++
+        }
+    }
+
+    for line, n := range counts { 
+        if n > 1 { 
+            fmt.Printf("%d\t%s\n", n, line)
+        }
+    }
+}
+
 func count_lines(f *os.File, counts map[string]int) {
     input := bufio.NewScanner(f)
     for input.Scan() {
@@ -62,5 +86,6 @@ func count_lines(f *os.File, counts map[string]int) {
 
 func main() {
     // find_duplicates1()
-    find_duplicates2()
+    // find_duplicates2()
+    find_duplicates3()
 }

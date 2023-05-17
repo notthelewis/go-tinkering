581dcf117dbc952619cc032f39e832e6f0194516
Lewis Farnworth <lewfar99@gmail.com>
2023-05-17 21:16:09 +0100

    Added fetch_all function, which uses io.Copy to efficiently copy data in a buffered way

--- book/1.5-fetching-url/main.go
+++ book/1.5-fetching-url/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func fetch_all() {
+    for _, url := range os.Args[1:] {
+        res, e := http.Get(url)
+
+        if e != nil {
+            fmt.Fprintf(os.Stderr, "fetch: %v\n", e)
+            os.Exit(1)
+        }
+
+        f, e := os.Create("./out.txt")
+        if e != nil {
+            fmt.Fprintf(os.Stderr, "fetch: opening file %v\n", e)
+        }
+
+        _, err := io.Copy(f, res.Body)
+        res.Body.Close()
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+            os.Exit(1)
+        }
+        fmt.Println("Finished")
+    }
+}
+
+func main() {
+    fetch_all()
+}
+

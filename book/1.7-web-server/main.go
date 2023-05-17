c8113fd69ec76f2e45ab65346abc8d3af2aae737
Lewis Farnworth <lewfar99@gmail.com>
2023-05-17 22:07:01 +0100

    ADded mutex example

--- book/1.7-web-server/main.go
+++ book/1.7-web-server/main.go
@@ -4,13 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+
+var mu sync.Mutex
+var count int
 
 func main() {
     http.HandleFunc("/", handler) 
+    http.HandleFunc("/count", counter)
     log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    mu.Lock()
+    count++
+    mu.Unlock()
     fmt.Fprintf(w, "url.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+    mu.Lock()
+    count++
+    fmt.Fprintf(w, "Count: %d\n", count)
+    mu.Unlock()
 }
